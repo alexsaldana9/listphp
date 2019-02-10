bee224c560b488b0dbff2048d095c6d890afe002
alexsaldana9 <alexsaldana9@gmail.com>
2019-02-09 23:00:33 -0500

    form added to post

--- index.php
+++ index.php
@@ -56,5 +56,29 @@ Homework 3: List of Favorites
                 </tr>
             <?php } ?>
         </table>
+        
+        <form method = "post" action = "index.php">
+            <div class="form-row">
+              <div class="form-group col-md-3">
+                <div class="form-group">
+                  <label for="start_date">Common Name:</label>
+                  <input type="text" class="form-control" id="common_name" name="common_name" value="<?php echo isset($_POST["common_name"]) ? $_POST["common_name"] : ''; ?>">
+                </div>
+                <div class="form-group">
+                  <label for="start_date">Scientific Name:</label>
+                  <input type="text" class="form-control" id="sci_name" name="sci_name" value="<?php echo isset($_POST["sci_name"]) ? $_POST["sci_name"] : ''; ?>">
+                </div>  
+                <div class="form-group">
+                  <label for="start_date">Native or Non-native:</label>
+                  <input type="text" class="form-control" id="type" name="type" value="<?php echo isset($_POST["type"]) ? $_POST["type"] : ''; ?>">
+                </div>
+              </div> 
+            </div>
+            <div class="form-row">
+                <div class="form-group">
+                    <input type="submit" value="add" class="btn btn-primary">
+                </div>
+            </div>
+        </form>
     </body>
 </html>
