02d90e7b0688545cdd533695ec3c3c78f80a89b8
Alexandra Saldana <alexsaldana9@gmail.com>
2019-03-12 22:27:36 -0400

    save the school database password

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,16 @@
 <?php
 function OpenCon() {
+  $isSchoolVm = false;
   $dbhost = "localhost";
   $dbuser = "admin";
   $dbpass = "b6k50RPHvGxj";
   $db = "favorites";
+
+  if ($isSchoolVm){
+  	$dbuser = "root";
+  	$dbpass = "password";
+  }
+
   $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
    
   return $conn;
